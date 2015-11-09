97906d18f26b075d4863b1a35a1b453d4ee838b5
Sam Caldwell <samuelgrantcaldwell@gmail.com>
2015-11-09 11:28:40 -0600

    more code

--- Senior Project/script.js
+++ Senior Project/script.js
@@ -1,32 +1,55 @@
-$(function () {
+$(document).ready(function () {
+     
+    // Declare variables for total amount of divs, and the current div. 
+    var divs = $(".divs > div"), divAmount = divs.length;
+    var currentDiv = 0;
     
-    var $divs = $(".divs > div"),
-        N = $divs.length,
-        C = 0;                   // Current    
+    // alert( currentDiv );
     
-    $divs.hide().eq(C).show();
+     var pageState = localStorage.getItem("pageState");
+     if (typeof pageState != "undefined") {
+    currentDiv = pageState;    
+    }
     
-    $("#next, #prev").click(function () {
-        $divs.stop().fadeOut().eq((this.id == 'next' ? ++C : --C) %N).fadeIn();
-    });
+    // alert( currentDiv );
     
-});
-
+        $(function () {
+                currentDiv;
+                divs.hide().eq(currentDiv).show();
+            
+            
+            $("#next").click(function (){ 
+                
+            })
+            
+            
+            $("#next, #prev").click(function () {
+                divs.stop().fadeOut().eq((this.id == 'next' ? currentDiv ++ : currentDiv--) %divAmount).fadeIn();
+                
+                
+        /* if (currentDiv > divAmount){
+            currentDiv = 0;
+            }
+        if (currentDiv < 0){
+            currentDiv = divAmount;
+            } */
+        console.log(currentDiv);
+        localStorage.setItem("pageState", currentDiv);
+                 
+        
+            }); // close click function
+        }); // close secondary function function
 
 var content = jSONtexts.texts;
-
-$(document).ready(function () {
-    
-    $("#content0").html(content[0].content);
-    $("#content1").html(content[1].content);
-    $("#content2").html(content[2].content);
-    $("#content3").html(content[3].content);
-    $("#content4").html(content[4].content);
-    $("#content5").html(content[5].content);
-    $("#content6").html(content[6].content);
-    $("#content7").html(content[7].content);
-    $("#content8").html(content[8].content);
-    
-});
-
-
+    $(document).ready(function () {
+        $("#content0").html(content[0].content);
+        $("#content1").html(content[1].content);
+        $("#content2").html(content[2].content);
+        $("#content3").html(content[3].content);
+        $("#content4").html(content[4].content);
+        $("#content5").html(content[5].content);
+        $("#content6").html(content[6].content);
+        $("#content7").html(content[7].content);
+        $("#content8").html(content[8].content);
+    }); //close json function
+}); // close main document
